d9b2b95575df6b64402e8d43088869b74bf0c9fa
tomandjerry <897778411@qq.com>
2018-11-26 20:35:57 +0800

    Initial commit

---
+++ SSM03-vo/src/main/java/com/xp/vo/Comment.java
@@ -0,0 +1,44 @@
+package com.xp.vo;
+
+
+public class Comment {
+    private Integer cId;
+
+    private Integer bId;
+
+    private String context;
+
+    private String commentDate;
+
+    public Integer getcId() {
+        return cId;
+    }
+
+    public void setcId(Integer cId) {
+        this.cId = cId;
+    }
+
+    public Integer getbId() {
+        return bId;
+    }
+
+    public void setbId(Integer bId) {
+        this.bId = bId;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context == null ? null : context.trim();
+    }
+
+    public String getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(String commentDate) {
+        this.commentDate = commentDate;
+    }
+}
---
+++ SSM03-vo/src/main/java/com/xp/vo/User.java
@@ -0,0 +1,65 @@
+package com.xp.vo;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+
+    private String userName;
+
+    private String nickName;
+
+    private String createDate;
+
+    private String lastUpdateDate;
+
+    private String userPwd;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(String lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd == null ? null : userPwd.trim();
+    }
+}
